5750e23e06b1d2f01a0be5bbcb279359c6484b27
scottruppel <scott.ruppel80@gmail.com>
2025-05-23 15:56:24 -0400

    Update __init__.py

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -2,16 +2,5 @@
 Analysis modules for market intelligence and feature engineering.
 """
 
-from .pattern_recognition import PatternRecognizer
-from .feature_engineering import AdvancedFeatureEngineer, SimpleFeatureEngineer
-from .dimensionality_reduction import DimensionalityReducer, QuickDimensionalityReducer
-from .temporal_analysis import TemporalAnalyzer
-
-__all__ = [
-    'PatternRecognizer',
-    'AdvancedFeatureEngineer',
-    'SimpleFeatureEngineer',
-    'DimensionalityReducer', 
-    'QuickDimensionalityReducer',
-    'TemporalAnalyzer'
-]
+# Don't import everything automatically
+__all__ = []
